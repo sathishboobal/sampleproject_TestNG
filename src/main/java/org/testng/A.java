1d125061e4278425e9ea0ef866cfcec3db92dc2e
sathishboobal <sathishboobal.sk@gmail.com>
2023-03-19 20:50:55 +0530

    A new preject added

---
+++ src/main/java/org/testng/A.java
@@ -0,0 +1,20 @@
+package org.testng;
+
+import org.junit.Test;
+
+public class A {
+	@Test
+	public void Tc20() {
+		System.out.println("TC20");
+	}
+	@Test
+	public void Tc22() {
+     System.out.println("TC22");
+	}
+     @Test
+	public void Tc25() {
+	System.out.println("TC25");
+
+	}
+
+}
